00711286b43092d5e33dce5af75536be8b1cbf4c
Paweł <pawdyslaw@gmail.com>
2023-10-13 16:13:44 +0200

    Zadanie 2_4

--- zadanie2_4/Program.cs
+++ zadanie2_4/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace zadanie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, tmp;
+            Console.WriteLine("Podaj a");
+            a = System.Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj b");
+            b = System.Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj c");
+            c = System.Convert.ToInt32(Console.ReadLine());
 
+            if (a > b)
+            {
+                tmp = a;
+                a = b;
+                b = tmp;
+            }
+            if (b > c)
+            {
+                tmp = b;
+                b = c;
+                c = tmp;
+            }
+            if (a > b)
+            {
+                tmp = a;
+                a = b;
+                b = tmp;
+            }
+
+            Console.WriteLine(a + "\t" + b + "\t" + c);
+        }
+    }
+}
