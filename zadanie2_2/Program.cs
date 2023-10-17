a1bc121fc82b4a03ff11185fc5efeb0ee8b30f56
Paweł <pawdyslaw@gmail.com>
2023-10-17 12:51:11 +0200

    Poprawa zadania 2_2

--- zadanie2_2/Program.cs
+++ zadanie2_2/Program.cs
@@ -6,7 +6,15 @@
         {
             Console.WriteLine("Podaj rok: ");
             int year = int.Parse(Console.ReadLine());
-            if (year == 0) { }
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) 
+            {
+                Console.WriteLine("Rok jest przestępny");
+            }
+            else
+            {
+                Console.WriteLine("Rok nie jest przestępny.");
+            }
+            
         }
     }
 }
