616ea1b9e2fc828872d7269ed19db5adffa35cbc
Paweł <pawdyslaw@gmail.com>
2023-11-07 11:22:06 +0100

    Zadanie 3_1

---
+++ zadanie3_1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace zadanie3_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int suma = 0;
+        int iterator = 0;
+        while (suma <= 100)
+        {
+            iterator++;
+            suma += iterator;
+        }
+        Console.WriteLine("Suma: " + suma);
+        Console.WriteLine("Ilość liczb dodanych: " + iterator);
+    }
+}
+
