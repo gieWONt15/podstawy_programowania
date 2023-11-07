41629f44601bcf2ba29f39ccc9b2b60686f94178
Paweł <pawdyslaw@gmail.com>
2023-11-07 12:23:15 +0100

    Zadanie 3_4

--- zadanie3_4/Program.cs
+++ zadanie3_4/Program.cs
@@ -4,7 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        double mojePi = 0;
+        for (int i = 0; i <= 100; i++)
+        {
+            mojePi += Math.Pow(-1, i) / (2 * i + 1);
+        }
+        Console.WriteLine(mojePi * 4);
+        Console.ReadLine();
     }
 }
 
