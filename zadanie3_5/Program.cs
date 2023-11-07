36840e6d98923ce1f715c9015ae662d7afde4e32
Paweł <pawdyslaw@gmail.com>
2023-11-07 11:55:19 +0100

    Dodanie zadania 3_5

---
+++ zadanie3_5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace zadanie3_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double kwota = 10000;
+        double procent = 0.05;
+
+        for (int i = 0; i < 12; i++)
+        {
+
+        }
+
+    }
+}
+// Kwota_inwestycji × (1 + Oprocentowanie / Liczba_okresów_kapitalizacji) ^ (Liczba_okresów_kapitalizacji × Okres_trwania_lokaty) – Kwota_inwestycji
