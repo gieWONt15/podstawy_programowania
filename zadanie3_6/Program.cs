5c1e9f5d33621d270c1ab32d48ed4b81ba8c7220
Paweł <pawdyslaw@gmail.com>
2023-11-07 12:51:07 +0100

    Zadanie 3_6

---
+++ zadanie3_6/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace zadanie3_6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj liczbę wyrazów ciągu Fibonacciego: ");
+        int max = Convert.ToInt32(Console.ReadLine());
+        int a = 0, b = 1, suma = 0;
+
+        for (int i = 0; i < max; i++)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+            suma += b;
+            Console.WriteLine(b);
+        }
+        Console.WriteLine($"Suma wynosi: {suma}");
+        Console.ReadLine();
+    }
+}
