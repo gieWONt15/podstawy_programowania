917532612bf40b64ebbba83657eec5fb47b496f6
Paweł <pawdyslaw@gmail.com>
2023-11-07 11:31:51 +0100

    Zadanie 3_2

---
+++ zadanie3_2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace zadanie3_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj liczbę do której będą sumowane ");
+        int max = Convert.ToInt32(Console.ReadLine());
+        
+        int iterator = 1;
+        int suma = 0;
+        while (iterator <= max)
+        {
+            suma += iterator;
+            iterator++;
+        }
+        Console.WriteLine("Suma: " + suma);
+        Console.ReadLine();
+    }
+}
