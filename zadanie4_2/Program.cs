9a444918450e53ac0224e4c31fbb0475270e9a58
Paweł <pawdyslaw@gmail.com>
2023-11-21 12:04:50 +0100

    Zadanie 4_2

---
+++ zadanie4_2/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                if (i + j == 11 | i == j | (i >= 4 & i <= 7 & j >= 4 & j <= 7))
+                {
+                    Console.Write(" * ");
+                }
+                else
+                {
+                    Console.Write("   ");
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}
