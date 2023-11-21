87b4bb558f71895436955507d4b18b2d8e01aa1b
Paweł <pawdyslaw@gmail.com>
2023-11-21 12:55:59 +0100

    Zadanie4_6

---
+++ zadanie4_6/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace zadanie4_6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int liczba = 1;
+
+        while (liczba <= 10000)
+        {
+            int sum = 0;
+            int j = 1;
+
+            while (j <= liczba /2)
+            {
+                if (liczba % j == 0)
+                {
+                    sum += j;
+                }
+                j++;
+            }
+            if (sum == liczba)
+            {
+                Console.WriteLine(liczba);
+            }
+            liczba++;
+        }
+        Console.ReadLine();
+    }
+}
+
