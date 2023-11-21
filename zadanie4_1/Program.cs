61edd56b791a3801baf5b6f9b54f5c06dbb13415
Paweł <pawdyslaw@gmail.com>
2023-11-21 11:36:04 +0100

    Zadanie4_1

---
+++ zadanie4_1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace zadanie4_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 0;
+
+        for (int i = 0; i <= 10; i++)
+        {
+            for (int j = 0; j <= 5; j++)
+            {
+                for (int k = 0; k <= 2; k++)
+                {
+                    if ((i + (j * 2) + (k * 5)) == 10)
+                    {
+                        x++;
+                        Console.WriteLine($"Kombinacja: {i} jednozłotówek, {j} dwuzłotówek, {k} pięciozłotówek");
+                    }
+                }
+            }
+        }
+        Console.WriteLine($"Liczba kombinacji: {x}");
+        Console.ReadLine();
+    }
+}
+
