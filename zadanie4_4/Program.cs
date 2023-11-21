354854cee84246211e2421b77aea2af7f5195853
Paweł <pawdyslaw@gmail.com>
2023-11-21 12:47:27 +0100

    Zadanie4_4

---
+++ zadanie4_4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace zadanie4_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 2; i <= 100; i++)
+        {
+            bool isPrime1 = true;
+            bool isPrime2 = true;
+
+            for (int j = 2; j <= Math.Sqrt(i) || j <= Math.Sqrt(i + 2); j++)
+            {
+                if (i % j == 0 && j <= Math.Sqrt(i))
+                {
+                    isPrime1 = false;
+                }
+                if ((i + 2) % j == 0 && j <=Math.Sqrt(i + 2))
+                {
+                    isPrime2 = false;
+                }
+
+                if (!isPrime1 && !isPrime2)
+                {
+                    break;
+                }
+            }
+            if (isPrime1 && isPrime2)
+            {
+                Console.WriteLine($"({i}, {i + 2})");
+            }
+        }
+        Console.ReadLine();
+    }
+}
+
