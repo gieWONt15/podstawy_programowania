1e5b42f319cbea1697b51524270013eb3256da2c
gieWONt15 <pawdyslaw@gmail.com>
2023-12-19 01:07:56 +0100

    Zadanie6_9

---
+++ zadanie6_9/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace zadanie6_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj tekst");
+            string tekst = Console.ReadLine();
+            Console.WriteLine("Wypisywanie liter:");
+            for (int i = 0; i < tekst.Length; i++)
+            {
+                Console.Write(tekst[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
