e3338af1d7500fbaf9077528d37a7bbc086f4db5
gieWONt15 <pawdyslaw@gmail.com>
2023-12-19 01:13:15 +0100

    Zadanie 6_10

---
+++ zadanie6_10/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace zadanie6_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj 5 tekstów");
+            string[] texts = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Podaj tekst {i + 1}: ");
+                texts[i] = Console.ReadLine();
+            }
+            string longest = texts[0];
+            for (int i = 0; i < 5; i++)
+            {
+                if (texts[i].Length > longest.Length)
+                {
+                    longest = texts[i];
+                }
+            }
+            Console.WriteLine($"Najdłuższy tekst to: {longest}");
+        }
+    }
+}
