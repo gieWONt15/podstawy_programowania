2ef44b544db84d4871d8c46df10fe1f868e8904f
gieWONt15 <pawdyslaw@gmail.com>
2024-01-03 22:44:18 +0100

    Zadanie7_1

---
+++ zadanie7_1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace zadanie7_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj gęstość zaludnienia A: (osoby/km^2");
+        double vol1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj gęstośc zaludnienia B: (osoby/km^2)");
+        double vol2 = double.Parse(Console.ReadLine());
+
+        double srednia = average(vol1, vol2);
+
+        Console.WriteLine($"Średnia gęsttość zaludnienia: {srednia} osoby/km^2");
+        Console.ReadLine();
+    }
+
+    static double average(double vol1, double vol2)
+    {
+        return 2 / ((1 / vol1) + (1 / vol2));
+    }
+}
