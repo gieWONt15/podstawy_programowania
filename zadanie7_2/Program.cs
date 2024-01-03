e671463dabcbdd425130f639fc07962631a76113
gieWONt15 <pawdyslaw@gmail.com>
2024-01-03 22:46:24 +0100

    Zadanie7_2

---
+++ zadanie7_2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace zadanie7_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.WriteLine($"Suma liczb od 1 do {i}: {SumUp(i)}");
+        }
+    }
+
+    static int SumUp(int num)
+    {
+        int sum = 0;
+        for (int i = 1; i <= num; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+}
