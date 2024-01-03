634a9cbeaf2c65d4b76f8ae3972af71c4b447c98
Paweł <pawdyslaw@gmail.com>
2024-01-04 00:41:28 +0100

    zadanie7_3

--- zadanie7_3/Program.cs
+++ zadanie7_3/Program.cs
@@ -4,7 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine($"2^{i} = {Power(i)}");
+        }
+
+        Console.WriteLine();
+
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine($"{i}! = {Factorial(i)}");
+        }
+        Console.ReadLine();
+    }
+
+    static long Power(long num)
+    {
+        if (num==0)
+        {
+            return 1;
+        }
+        else
+        {
+            return (long)Math.Pow(2, num);
+        }
+    }
+
+    static long Factorial(long num)
+    {
+        if (num==0 || num==1)
+        {
+            return 1;
+        }
+        else
+        {
+            return num * Factorial(num - 1);
+        }
     }
 }
 
