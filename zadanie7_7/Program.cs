bd53502cb6fd70b4fd3c88000f0c6f2e79fbb261
Paweł <pawdyslaw@gmail.com>
2024-01-04 22:56:44 +0100

    zadanie7_7

---
+++ zadanie7_7/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace zadanie7_7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Liczby bliźniacze w przedziale <200, 400>");
+        for (int i = 200; i <= 400; i++)
+        {
+            if (CzyBliźniacze(i, i+2))
+            {
+                Console.WriteLine($"{i} i {i+2}");
+            }
+        }
+        Console.ReadKey();
+    }
+
+    static bool CzyLiczbaPierwsza(int liczba)
+    {
+        if (liczba <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(liczba); i++)
+        {
+            if (liczba % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static bool CzyBliźniacze(int liczba1, int liczba2)
+    {
+        return CzyLiczbaPierwsza(liczba1) && CzyLiczbaPierwsza(liczba2);
+    }
+}
+
